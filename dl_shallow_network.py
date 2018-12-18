b9022385ee18dacecc5a80e6d28860be42d34a05
Krutarth Rao <krutarth.rao@hpe.com>
2018-12-17 18:41:00 -0800

    linting

--- dl_shallow_network.py
+++ dl_shallow_network.py
@@ -1,13 +1,11 @@
 
-
+from collections import namedtuple
 import numpy as np
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
-
-from collections import namedtuple
 
 Data = namedtuple('Data', ['x', 'y'])
 
@@ -17,7 +15,7 @@ class ShallowNet:
         Shallow Neural Network in Keras
 
             A shallow neural network to classify MNIST digits. MNIST dataset is a set
-            of 28x28 pixel images that contain handwritten images.
+            of 28x28 pixel images that contain images of handwritten numbers.
 
             Develop a shallow neural network to recognize the number in the image.
 
@@ -81,7 +79,7 @@ class ShallowNet:
 
     def make_model(self):
         '''
-            Make model with 2 dense layers. The output
+            Make model with 1 dense layer. The output
             of EACH neuron goes as input to every neuron
             in the next layre.
 
@@ -92,19 +90,42 @@ class ShallowNet:
         layer_1_neurons = 64
         out_classes = 10
         learning_rate = 0.01
-        model.add(Dense(layer_1_neurons, activation='sigmoid', input_shape=(self.num_pixels,)))
-        model.add(Dense(out_classes, activation='softmax'))
-        model.compile(loss='mean_squared_error', optimizer=SGD(lr=learning_rate), metrics=['accuracy'])
+        model.add(
+            Dense(
+                layer_1_neurons,
+                activation='sigmoid',
+                input_shape=(self.num_pixels,)
+            )
+        )
+        model.add(
+            Dense(
+                out_classes,
+                activation='softmax'
+            )
+        )
+        model.compile(
+            loss='mean_squared_error',
+            optimizer=SGD(lr=learning_rate),
+            metrics=['accuracy']
+        )
         print(model.summary())
         self.model = model
 
     def run(self):
-        self.model.fit(self.train_data.x, self.train_data.y, batch_size=128, epochs=2,
-                  verbose=1, validation_data=(self.test_data.x, self.test_data.y))
+        '''
+            Train the model on the data and evaluate
+            using test datasets
+        '''
+        self.model.fit(
+            self.train_data.x,
+            self.train_data.y,
+            batch_size=15,
+            epochs=2,
+            verbose=1,
+            validation_data=(self.test_data.x, self.test_data.y)
+        )
         res = self.model.evaluate(self.test_data.x, self.test_data.y)
         print('Loss, Accuracy: ', res)
-
-
 
 
 def main():
